8c00bc8a2edddf25004bf16517444e5a6e41590c
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-05-06 23:36:53 +0100

    authenticated navbar view

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { MdClose, MdMenu } from "react-icons/md";
 import { Link } from "react-router-dom";
-import Logo from "../partials/Logo";
-import Button from "../partials/Button";
+import Logo from "../Logo";
+import Button from "../Button";
 import { GoArrowRight } from "react-icons/go";
+import { AuthContext } from "../../context/AuthContext";
+import Dropdown from "../Dropdown";
 
 export default function Navbar() {
   const [openMenu, setOpenMenu] = useState(false);
+  const { currentUser } = useContext(AuthContext);
 
   const closeMenu = () => {
     setOpenMenu(!openMenu);
@@ -14,35 +17,49 @@ export default function Navbar() {
 
   return (
     <nav className={"sticky w-full z-50 max-w-[1200px] mx-auto px-2"}>
-      <div className="h-15 place-content-center">
-        <div className="flex items-center justify-between w-full">
-          <Logo />
+      <div className="min-h-15 mt-2 sm:mt-0 place-content-center">
+        <div className="flex flex-col gap-2 mb-4 sm:m-0 sm:flex-row items-center justify-between w-full">
+          <Link to={!currentUser.email ? "/" : "/dashboard"}>
+            <Logo />
+          </Link>
 
-          <div className="hidden md:flex gap-2">
-            <Link to={"/login"}>
-              <Button btnText={"Log in"} variant={"outline"} />
-            </Link>
-            <Link to={"/signup"}>
-              <Button
-                btnText={
-                  <div className="flex items-center gap-1">
-                    Sign up <GoArrowRight />
-                  </div>
-                }
-                variant={"primary"}
-              />
-            </Link>
-          </div>
+          {!currentUser && (
+            <div className="hidden md:flex gap-2">
+              <Link to={"/login"}>
+                <Button btnText={"Log in"} variant={"outline"} />
+              </Link>
+              <Link to={"/signup"}>
+                <Button
+                  btnText={
+                    <div className="flex items-center gap-1">
+                      Sign up <GoArrowRight />
+                    </div>
+                  }
+                  variant={"primary"}
+                />
+              </Link>
+            </div>
+          )}
 
-          <div className="md:hidden">
-            {!openMenu ? (
-              <MdMenu size={24} onClick={closeMenu} />
-            ) : (
-              <MdClose size={24} onClick={closeMenu} />
-            )}
-          </div>
+          {!currentUser && (
+            <div className="md:hidden">
+              {!openMenu ? (
+                <MdMenu size={24} onClick={closeMenu} />
+              ) : (
+                <MdClose size={24} onClick={closeMenu} />
+              )}
+            </div>
+          )}
+          {currentUser && (
+            <Dropdown
+              text={currentUser.email}
+              itemPosiiton="dropdown-bottom dropdown-center sm:dropdown-end"
+              items={["Log Out"]}
+            />
+          )}
         </div>
       </div>
+
       {openMenu && (
         <div className="md:hidden bg-gray-100">
           <div className="grid place-items-center py-5 gap-2 font-medium">
